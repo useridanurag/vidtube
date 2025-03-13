fe6575cdf0287658fb95b0bc6d6b805556d4f51f
ANURAG <forconnectanurag@gmail.com>
2025-03-14 05:25:30 +0530

    Setup utils codes

--- vidtube-backend/index.js
+++ vidtube-backend/index.js
@@ -1,18 +1,14 @@
 import dotenv from "dotenv";
-import express from "express";
 import connectDB from "./src/config/db.js";
+import {app} from "express";
 
 dotenv.config();
-
-//Variables
-const app = express();
-const PORT = process.env.PORT || 4000;
 
 //DB connection
 connectDB()
   .then(() => {
     app.listen(PORT, () => {
-      console.log(`Server is listening on port : ${PORT}`);
+      console.log(`Server is listening on port : ${process.env.PORT || 4000}`);
     })
   })
   .catch((error) => {
