caed21f1b89d0bcf927df38dd21e6b9575a7fd95
ANURAG <forconnectanurag@gmail.com>
2025-03-20 03:41:12 +0530

    fixing logout controller

--- vidtube-backend/src/controllers/user.controller.js
+++ vidtube-backend/src/controllers/user.controller.js
@@ -110,8 +110,8 @@ const logout = asyncHandler(async (req, res) => {
     secure: true,
   }
   return res.status(200)
-    .clearCookie(refreshToken, options)
-    .clearCookie(accessToken, options)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
     .json(new ApiResponse(200, {}, "User Logged out."));
 
 })
